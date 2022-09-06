ba36d55e757e40ba03fca68021638861b11b91d8
Jay McCormick <jayexprs@gmail.com>
2022-09-06 15:31:24 -0400

    installed solid js, got first solid component working

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+function MenuItem(props){
+  const item = () => props.item;
+  return <a>{item()}</a>
+}
+
+function TopMenu(props){
+  const topMenu = ['Getting started', 'Components', 'Styles', 'Layouts', 'Patterns', 'Use cases']
+  return (
+    <div>
+      <For each={topMenu}>{(item) =>
+        <MenuItem item={item}></MenuItem>
+      }
+        </For>
+    </div>
+  )
+}
+
+export default TopMenu
